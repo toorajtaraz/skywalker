d25a337b8cc7cdafd130268374d99f22ecf555a2
tooraj taraz <tooraj.info@gmail.com>
2020-11-20 13:35:59 +0330

    impementing main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod port_sniffer;
+mod handle_arg;
+
+
 fn main() {
-    println!("Hello, world!");
+   match handle_arg::get_args() {
+        Ok(o) => port_sniffer::run(o.threads.unwrap(), o.ipaddr.unwrap(), o.timeout.unwrap(), o.verbose.unwrap()),
+        Err(e) => println!("{}", e) 
+   } 
 }
