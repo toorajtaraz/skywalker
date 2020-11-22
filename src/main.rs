3f86b022abe139b95453299fcc47222366bd9f41
tooraj taraz <tooraj.info@gmail.com>
2020-11-23 02:10:37 +0330

    final changes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-mod port_sniffer;
-mod handle_arg;
-
+mod util;
+use util::port_sniffer;
+use util::handle_arg;
+use util::packet_sniffer;
+use util::packet_parser;
 
 fn main() {
    match handle_arg::get_args() {
-        Ok(o) => port_sniffer::run(o.threads.unwrap(), o.ipaddr.unwrap(), o.timeout.unwrap(), o.verbose.unwrap()),
-        Err(e) => println!("{}", e) 
+        Ok(args) => {
+            match args.mode {
+                handle_arg::Modes::PortSniff => {
+                    port_sniffer::run(args.threads.unwrap(), args.ipaddr.unwrap(), args.timeout.unwrap(), args.verbose.unwrap())
+                },
+                handle_arg::Modes::Capture => {
+                    packet_sniffer::listen(args.index.unwrap(), args.verbose.unwrap())
+                },
+                handle_arg::Modes::PrintDevices => {
+                    packet_sniffer::print_devices()
+                }
+            }
+        },
+            Err(e) => println!("{}", ansi_term::Color::Red.bold().paint(format!("{}", e).as_str())) 
    } 
 }
