ee4349d1d7d0046f8bd23445b8379f61b8f333df
tooraj taraz <tooraj.info@gmail.com>
2021-02-18 11:41:09 +0330

    formatting main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,45 @@
 mod util;
-use util::port_sniffer;
 use util::handle_arg;
-use util::packet_sniffer;
-use util::packet_parser;
 use util::handle_icmp_ping;
 use util::handle_icmp_traceroute;
+use util::packet_parser;
+use util::packet_sniffer;
+use util::port_sniffer;
 fn main() {
-   match handle_arg::get_args() {
-        Ok(args) => {
-            match args.mode {
-                handle_arg::Modes::PortSniff => {
-                    port_sniffer::run(args.threads.unwrap(), args.ipaddr.unwrap(), args.timeout.unwrap(), args.verbose.unwrap(), args.famous.unwrap())
-                },
-                handle_arg::Modes::Capture => {
-                    packet_sniffer::listen(args.index.unwrap(), args.verbose.unwrap())
-                },
-                handle_arg::Modes::PrintDevices => {
-                    packet_sniffer::print_devices()
-                },
-                handle_arg::Modes::Ping => {
-                    handle_icmp_ping::ping_hosts(args.hosts.unwrap());
-                },
-                handle_arg::Modes::TraceRoute => {
-                    handle_icmp_traceroute::traceroute(args.max_ttl, args.start_ttl, args.max_tries, args.timeout, args.port, args.size, args.host.unwrap(), args.protocol);
-                }
+    match handle_arg::get_args() {
+        Ok(args) => match args.mode {
+            handle_arg::Modes::PortSniff => port_sniffer::run(
+                args.threads.unwrap(),
+                args.ipaddr.unwrap(),
+                args.timeout.unwrap(),
+                args.verbose.unwrap(),
+                args.famous.unwrap(),
+            ),
+            handle_arg::Modes::Capture => {
+                packet_sniffer::listen(args.index.unwrap(), args.verbose.unwrap())
+            }
+            handle_arg::Modes::PrintDevices => packet_sniffer::print_devices(),
+            handle_arg::Modes::Ping => {
+                handle_icmp_ping::ping_hosts(args.hosts.unwrap());
+            }
+            handle_arg::Modes::TraceRoute => {
+                handle_icmp_traceroute::traceroute(
+                    args.max_ttl,
+                    args.start_ttl,
+                    args.max_tries,
+                    args.timeout,
+                    args.port,
+                    args.size,
+                    args.host.unwrap(),
+                    args.protocol,
+                );
             }
         },
-            Err(e) => println!("{}", ansi_term::Color::Red.bold().paint(format!("{}", e).as_str())) 
-   } 
+        Err(e) => println!(
+            "{}",
+            ansi_term::Color::Red
+                .bold()
+                .paint(format!("{}", e).as_str())
+        ),
+    }
 }
