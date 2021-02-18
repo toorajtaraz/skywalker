d9b7bbe586b7da0bf7cebd16859e1e9935acfa6e
tooraj taraz <tooraj.info@gmail.com>
2021-02-18 11:41:45 +0330

    formatting arg handler

--- src/util/handle_arg.rs
+++ src/util/handle_arg.rs
@@ -1,21 +1,21 @@
 /*[>
 
 
-         __                        ____            
+         __                        ____
    _____/ /____  ___      ______ _/ / /_____  _____
   / ___/ //_/ / / / | /| / / __ `/ / //_/ _ \/ ___/
- (__  ) ,< / /_/ /| |/ |/ / /_/ / / ,< /  __/ /    
-/____/_/|_|\__, / |__/|__/\__,_/_/_/|_|\___/_/     
-          /____/                                   
+ (__  ) ,< / /_/ /| |/ |/ / /_/ / / ,< /  __/ /
+/____/_/|_|\__, / |__/|__/\__,_/_/_/|_|\___/_/
+          /____/
 
 
 
 <]*/
 extern crate clap;
-use clap::{Arg, App};
+use clap::{App, Arg};
+use std::fmt;
 use std::net::IpAddr;
 use std::str::FromStr;
-use std::fmt;
 extern crate ansi_term;
 use ansi_term::Colour::RGB;
 
@@ -53,7 +53,6 @@ impl fmt::Display for Arguments {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         writeln!(f, "Arguments include: ip address: {:?}, threads: {:?}, time out: {:?} and verbosity: {:?}", self.ipaddr, self.threads, self.timeout, self.verbose)
     }
-
 }
 
 pub fn get_args() -> Result<Arguments, String> {
@@ -159,75 +158,81 @@ pub fn get_args() -> Result<Arguments, String> {
     let hosts = matches.value_of("ping").unwrap_or("was not provided");
     let host = matches.value_of("trace").unwrap_or("was not provided");
     if !host.contains("was not provided") {
-        let mut args =  Arguments{threads: None, timeout: None, verbose: None, ipaddr: None, mode: Modes::TraceRoute, index: None, famous: None, hosts: None, max_ttl: None, start_ttl: None, max_tries: None, port: None, size: None, host: None, protocol: None};
+        let mut args = Arguments {
+            threads: None,
+            timeout: None,
+            verbose: None,
+            ipaddr: None,
+            mode: Modes::TraceRoute,
+            index: None,
+            famous: None,
+            hosts: None,
+            max_ttl: None,
+            start_ttl: None,
+            max_tries: None,
+            port: None,
+            size: None,
+            host: None,
+            protocol: None,
+        };
         match matches.value_of("max_tries") {
-            Some(max_tries) => {
-                match max_tries.parse::<u16>() {
-                    Ok(max_tries) => {
-                        args.max_tries = Some(max_tries);
-                    }
-                    Err(_) => {
-                        err.push_str(format!("max_tries must be unsigned 16 bit integer.").as_str());
-                        return Err(err);
-                    }
+            Some(max_tries) => match max_tries.parse::<u16>() {
+                Ok(max_tries) => {
+                    args.max_tries = Some(max_tries);
                 }
-            }
+                Err(_) => {
+                    err.push_str(format!("max_tries must be unsigned 16 bit integer.").as_str());
+                    return Err(err);
+                }
+            },
             _ => {}
         }
         match matches.value_of("max_ttl") {
-            Some(max_ttl) => {
-                match max_ttl.parse::<u8>() {
-                    Ok(max_ttl) => {
-                        args.max_ttl = Some(max_ttl);
-                    }
-                    Err(_) => {
-                        err.push_str(format!("max_ttl must be unsigned 8 bit integer.").as_str());
-                        return Err(err);
-                    }
+            Some(max_ttl) => match max_ttl.parse::<u8>() {
+                Ok(max_ttl) => {
+                    args.max_ttl = Some(max_ttl);
                 }
-            }
+                Err(_) => {
+                    err.push_str(format!("max_ttl must be unsigned 8 bit integer.").as_str());
+                    return Err(err);
+                }
+            },
             _ => {}
         }
         match matches.value_of("start_ttl") {
-            Some(start_ttl) => {
-                match start_ttl.parse::<u8>() {
-                    Ok(start_ttl) => {
-                        args.start_ttl = Some(start_ttl);
-                    }
-                    Err(_) => {
-                        err.push_str(format!("start_ttl must be unsigned 8 bit integer.").as_str());
-                        return Err(err);
-                    }
+            Some(start_ttl) => match start_ttl.parse::<u8>() {
+                Ok(start_ttl) => {
+                    args.start_ttl = Some(start_ttl);
                 }
-            }
+                Err(_) => {
+                    err.push_str(format!("start_ttl must be unsigned 8 bit integer.").as_str());
+                    return Err(err);
+                }
+            },
             _ => {}
         }
         match matches.value_of("port") {
-            Some(port) => {
-                match port.parse::<u16>() {
-                    Ok(port) => {
-                        args.port = Some(port);
-                    }
-                    Err(_) => {
-                        err.push_str(format!("port must be unsigned 16 bit integer.").as_str());
-                        return Err(err);
-                    }
+            Some(port) => match port.parse::<u16>() {
+                Ok(port) => {
+                    args.port = Some(port);
                 }
-            }
+                Err(_) => {
+                    err.push_str(format!("port must be unsigned 16 bit integer.").as_str());
+                    return Err(err);
+                }
+            },
             _ => {}
         }
         match matches.value_of("size") {
-            Some(size) => {
-                match size.parse::<usize>() {
-                    Ok(size) => {
-                        args.size = Some(size);
-                    }
-                    Err(_) => {
-                        err.push_str(format!("size be unsigned integer.").as_str());
-                        return Err(err);
-                    }
+            Some(size) => match size.parse::<usize>() {
+                Ok(size) => {
+                    args.size = Some(size);
                 }
-            }
+                Err(_) => {
+                    err.push_str(format!("size be unsigned integer.").as_str());
+                    return Err(err);
+                }
+            },
             _ => {}
         }
         match matches.value_of("protocol") {
@@ -244,17 +249,15 @@ pub fn get_args() -> Result<Arguments, String> {
             _ => {}
         }
         match matches.value_of("timeout_trace") {
-            Some(timeout) => {
-                match timeout.parse::<u32>() {
-                    Ok(timeout) => {
-                        args.timeout = Some(timeout);
-                    }
-                    Err(_) => {
-                        err.push_str(format!("timeout must be unsigned integer.").as_str());
-                        return Err(err);
-                    }
+            Some(timeout) => match timeout.parse::<u32>() {
+                Ok(timeout) => {
+                    args.timeout = Some(timeout);
                 }
-            }
+                Err(_) => {
+                    err.push_str(format!("timeout must be unsigned integer.").as_str());
+                    return Err(err);
+                }
+            },
             _ => {}
         }
         match matches.value_of("trace").unwrap().parse::<IpAddr>() {
@@ -277,7 +280,7 @@ pub fn get_args() -> Result<Arguments, String> {
                 err.push_str(format!("Bad host.").as_str());
                 return Err(err);
             }
-        }; 
+        };
     }
     let mut host_list: Vec<IpAddr> = Vec::new();
     if !hosts.contains("was not provided") {
@@ -287,7 +290,10 @@ pub fn get_args() -> Result<Arguments, String> {
             }
             match host.parse::<IpAddr>() {
                 Ok(ip) => {
-                    println!("IP<{}> added for being pinged...", RGB(223, 97, 0).paint(format!("{}", ip)));
+                    println!(
+                        "IP<{}> added for being pinged...",
+                        RGB(223, 97, 0).paint(format!("{}", ip))
+                    );
                     host_list.push(ip);
                     continue;
                 }
@@ -296,7 +302,11 @@ pub fn get_args() -> Result<Arguments, String> {
             match (&(format!("{}:0", host))[..]).to_socket_addrs() {
                 Ok(sockaddrs) => {
                     for sa in sockaddrs {
-                        println!("Host<{}><{}> added for being pinged...", RGB(102, 255, 255).paint(format!("{:?}", host)), RGB(223, 97, 0).paint(format!("{}", sa.ip())));
+                        println!(
+                            "Host<{}><{}> added for being pinged...",
+                            RGB(102, 255, 255).paint(format!("{:?}", host)),
+                            RGB(223, 97, 0).paint(format!("{}", sa.ip()))
+                        );
                         host_list.push(sa.ip());
                     }
                     continue;
@@ -304,14 +314,30 @@ pub fn get_args() -> Result<Arguments, String> {
                 Err(_) => {
                     println!("Bad input ignoring {}", host);
                 }
-            }; 
+            };
         }
-        return Ok(Arguments{threads: None, timeout: None, verbose: None, ipaddr: None, mode: Modes::Ping, index: None, famous: None, hosts: Some(host_list), max_ttl: None, start_ttl: None, max_tries: None, port: None, size: None, host: None, protocol: None});
+        return Ok(Arguments {
+            threads: None,
+            timeout: None,
+            verbose: None,
+            ipaddr: None,
+            mode: Modes::Ping,
+            index: None,
+            famous: None,
+            hosts: Some(host_list),
+            max_ttl: None,
+            start_ttl: None,
+            max_tries: None,
+            port: None,
+            size: None,
+            host: None,
+            protocol: None,
+        });
     }
     let cpu = format!("{}", get());
     let cpu = cpu.as_str();
     let threads = matches.value_of("threads").unwrap_or(cpu);
-    let timeout = matches.value_of("timeout").unwrap_or("1000000000");    
+    let timeout = matches.value_of("timeout").unwrap_or("1000000000");
     let ip = matches.value_of("IP").unwrap_or("was not provided");
     let index = matches.value_of("capture").unwrap_or("was not provided");
     let verbosity = match matches.occurrences_of("verbose") {
@@ -320,16 +346,46 @@ pub fn get_args() -> Result<Arguments, String> {
         2 => 2,
         3 | _ => 3,
     };
-    if ip.contains("was") && !index.contains("was"){
+    if ip.contains("was") && !index.contains("was") {
         let index = match index.parse::<u16>() {
-            Ok(num) => {
-                num
-            },
-            _ => 0
+            Ok(num) => num,
+            _ => 0,
         };
-        return Ok(Arguments{threads: None, timeout: None, verbose: Some(verbosity), ipaddr: None, mode: Modes::Capture, index: Some(index), famous: None, hosts: None, max_ttl: None, start_ttl: None, max_tries: None, port: None, size: None, host: None, protocol: None});
+        return Ok(Arguments {
+            threads: None,
+            timeout: None,
+            verbose: Some(verbosity),
+            ipaddr: None,
+            mode: Modes::Capture,
+            index: Some(index),
+            famous: None,
+            hosts: None,
+            max_ttl: None,
+            start_ttl: None,
+            max_tries: None,
+            port: None,
+            size: None,
+            host: None,
+            protocol: None,
+        });
     } else if ip.contains("was") && index.contains("was") && matches.is_present("listDev") {
-        return Ok(Arguments{threads: None, timeout: None, verbose: None, ipaddr: None, mode: Modes::PrintDevices, index: None, famous: None, hosts: None, max_ttl: None, start_ttl: None, max_tries: None, port: None, size: None, host: None, protocol: None});
+        return Ok(Arguments {
+            threads: None,
+            timeout: None,
+            verbose: None,
+            ipaddr: None,
+            mode: Modes::PrintDevices,
+            index: None,
+            famous: None,
+            hosts: None,
+            max_ttl: None,
+            start_ttl: None,
+            max_tries: None,
+            port: None,
+            size: None,
+            host: None,
+            protocol: None,
+        });
     } else if ip.contains("was") {
         err.push_str("no parameters were provided.");
         return Err(err);
@@ -342,7 +398,7 @@ pub fn get_args() -> Result<Arguments, String> {
             } else {
                 num
             }
-        },
+        }
         _ => {
             let threads = threads.clone();
             let threads = format!("could not parse <{}> to integer.", threads.clone());
@@ -357,22 +413,36 @@ pub fn get_args() -> Result<Arguments, String> {
             } else {
                 num
             }
-        },
+        }
         _ => {
             let timeout = timeout.clone();
             let timeout = format!("could not parse <{}> to integer.", timeout.clone());
             return Err(timeout);
-        } 
+        }
     };
     let ip = match IpAddr::from_str(ip) {
-        Ok(addr) => {
-            addr
-        },
+        Ok(addr) => addr,
         Err(_) => {
             let ip = format!("could not parse <{}> to ipv4/v6.", ip);
             return Err(ip);
-        } 
+        }
     };
 
-    Ok(Arguments{threads: Some(threads), timeout: Some(timeout), verbose: Some(verbosity), ipaddr: Some(ip), mode: Modes::PortSniff, index: None, famous: Some(matches.is_present("famous")), hosts: None, max_ttl: None, start_ttl: None, max_tries: None, port: None, size: None, host: None, protocol: None})
+    Ok(Arguments {
+        threads: Some(threads),
+        timeout: Some(timeout),
+        verbose: Some(verbosity),
+        ipaddr: Some(ip),
+        mode: Modes::PortSniff,
+        index: None,
+        famous: Some(matches.is_present("famous")),
+        hosts: None,
+        max_ttl: None,
+        start_ttl: None,
+        max_tries: None,
+        port: None,
+        size: None,
+        host: None,
+        protocol: None,
+    })
 }
