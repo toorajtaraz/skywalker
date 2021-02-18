ac81350e6a0ada83cecbb4b140dce25b142d9d2f
tooraj taraz <tooraj.info@gmail.com>
2021-02-18 11:43:10 +0330

    formatting sniffer utils

--- src/util/packet_sniffer.rs
+++ src/util/packet_sniffer.rs
@@ -2,63 +2,76 @@
 extern crate pcap;
 use pcap::Device;
 extern crate ansi_term;
+use crate::packet_parser::{self, parse_packet};
 use ansi_term::Color;
-use crate::packet_parser::{parse_packet, self};
 
 pub fn print_devices() {
-    let mut i : u16 = 1;
+    let mut i: u16 = 1;
     match pcap::Device::list() {
         Ok(dev) => {
             for device in dev {
                 let device_des = device.desc;
                 println!("-{} DEV NAME: {}", i, device.name);
                 match device_des {
-                    Some(d) => println!("\t{}", Color::Blue.paint(format!("DEV DESCRIPTION: {}", d).as_str())),
-                    None => println!("\t{}", Color::Red.paint("DEV HAD NO DESCRIPTION."))
+                    Some(d) => println!(
+                        "\t{}",
+                        Color::Blue.paint(format!("DEV DESCRIPTION: {}", d).as_str())
+                    ),
+                    None => println!("\t{}", Color::Red.paint("DEV HAD NO DESCRIPTION.")),
                 }
                 i += 1;
             }
-        },
+        }
         Err(_) => {
-            println!("{}", Color::Red.paint("something went wrong while getting device list."));
+            println!(
+                "{}",
+                Color::Red.paint("something went wrong while getting device list.")
+            );
             return;
         }
     }
-    println!("{}", Color::Green.paint(format!("{} DEVS WERE FOUND!", i - 1).as_str()))
+    println!(
+        "{}",
+        Color::Green.paint(format!("{} DEVS WERE FOUND!", i - 1).as_str())
+    )
 }
 
 fn capture(dev: Device, verbosity: u8, process: bool) {
     match pcap::Capture::from_device(dev) {
-        Ok(capture) => {
-            match capture.open() {
-                Ok(mut opened) => {
-                    loop {
-                        match opened.next() {
-                            Ok(packet) => {
-                                if !process {
-                                   packet_parser::print_raw(packet.header.len, format!("{}.{:06}", &packet.header.ts.tv_sec, &packet.header.ts.tv_usec), packet.data.to_owned(), verbosity);
-                                   continue;
-                                }
-                                let data = packet.data.to_owned();
-                                let len = packet.header.len;
-                                let ts : String = format!(
-                                        "{}.{:06}",
-                                        &packet.header.ts.tv_sec, &packet.header.ts.tv_usec
-                                    );
-                                let packet = parse_packet(data , len, ts);
-                                packet.print(verbosity);
-                            },
-                            _ => {
-                                println!("{}", Color::Red.paint("something went wrong while capturing packets. please make sure you are executing this command as sudo.(NOT FOR DBUS, USE NORMAL USER FOR DBUS)"));
-                                return;
-                            }
+        Ok(capture) => match capture.open() {
+            Ok(mut opened) => loop {
+                match opened.next() {
+                    Ok(packet) => {
+                        if !process {
+                            packet_parser::print_raw(
+                                packet.header.len,
+                                format!(
+                                    "{}.{:06}",
+                                    &packet.header.ts.tv_sec, &packet.header.ts.tv_usec
+                                ),
+                                packet.data.to_owned(),
+                                verbosity,
+                            );
+                            continue;
                         }
+                        let data = packet.data.to_owned();
+                        let len = packet.header.len;
+                        let ts: String = format!(
+                            "{}.{:06}",
+                            &packet.header.ts.tv_sec, &packet.header.ts.tv_usec
+                        );
+                        let packet = parse_packet(data, len, ts);
+                        packet.print(verbosity);
                     }
-                },
-                _ => {
-                    println!("{}", Color::Red.paint("something went wrong while capturing packets.  please make sure you are executing this command as sudo.(NOT FOR DBUS, USE NORMAL USER FOR DBUS)"));
-                    return;
+                    _ => {
+                        println!("{}", Color::Red.paint("something went wrong while capturing packets. please make sure you are executing this command as sudo.(NOT FOR DBUS, USE NORMAL USER FOR DBUS)"));
+                        return;
+                    }
                 }
+            },
+            _ => {
+                println!("{}", Color::Red.paint("something went wrong while capturing packets.  please make sure you are executing this command as sudo.(NOT FOR DBUS, USE NORMAL USER FOR DBUS)"));
+                return;
             }
         },
         _ => {
@@ -72,34 +85,53 @@ pub fn listen(index: u16, verbosity: u8) {
     let devices = match Device::list() {
         Ok(devs) => devs,
         Err(_) => {
-            println!("{}", Color::Red.paint("something went wrong while getting device list for capturing packets."));
+            println!(
+                "{}",
+                Color::Red
+                    .paint("something went wrong while getting device list for capturing packets.")
+            );
             return;
         }
     };
     if (devices.len() as u16) < index {
-        println!("{}", Color::Red.paint(format!("{} was not found in list.", index).as_str())); 
+        println!(
+            "{}",
+            Color::Red.paint(format!("{} was not found in list.", index).as_str())
+        );
         return;
     }
     println!("{}", Color::Red.bold().paint("LISTENING..."));
     let mut count = 1;
     for dev in devices {
-       if index != 0 && count == index {
-            if !(dev.name.contains("wl") || dev.name.contains("eth") || dev.name.contains("ppp") || dev.name.contains("any") || dev.name.contains("en")) {
+        if index != 0 && count == index {
+            if !(dev.name.contains("wl")
+                || dev.name.contains("eth")
+                || dev.name.contains("ppp")
+                || dev.name.contains("any")
+                || dev.name.contains("en"))
+            {
                 capture(dev, verbosity, false);
                 return;
             }
             capture(dev, verbosity, true);
             return;
-       } else if index == 0 {
-           if dev.name.contains("wl") || dev.name.contains("eth") || dev.name.contains("ppp") || dev.name.contains("any") || dev.name.contains("en") {
+        } else if index == 0 {
+            if dev.name.contains("wl")
+                || dev.name.contains("eth")
+                || dev.name.contains("ppp")
+                || dev.name.contains("any")
+                || dev.name.contains("en")
+            {
                 capture(dev, verbosity, true);
-                return; 
-           }
-           continue;
-       }
-    count += 1;
-    continue;
-    
+                return;
+            }
+            continue;
+        }
+        count += 1;
+        continue;
     }
-    println!("{}", Color::Red.bold().paint("NO SUITABLE INTERFACE WERE FOUND!"));
+    println!(
+        "{}",
+        Color::Red.bold().paint("NO SUITABLE INTERFACE WERE FOUND!")
+    );
 }
--- src/util/port_sniffer.rs
+++ src/util/port_sniffer.rs
@@ -1,64 +1,74 @@
-use std::io::{self, Write};
-use std::net::{IpAddr, TcpStream, SocketAddr};
-use std::sync::mpsc::{Sender, channel};
-use std::thread;
 use ansi_term::Color;
+use std::io::{self, Write};
+use std::net::{IpAddr, SocketAddr, TcpStream};
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
 
 const MAX: u16 = 65353;
 const FAMOUS: u16 = 1024;
 
-fn scan(tx: Sender<String>, id: u16, steps:u16, addr: IpAddr, timeout: u32, verbosity: u8, famous: bool) {
+fn scan(
+    tx: Sender<String>,
+    id: u16,
+    steps: u16,
+    addr: IpAddr,
+    timeout: u32,
+    verbosity: u8,
+    famous: bool,
+) {
     let port: u16 = id * steps;
     for i in 1..=steps {
-        if port > MAX || (famous && port > FAMOUS){         
+        if port > MAX || (famous && port > FAMOUS) {
             break;
         }
-        let sa = SocketAddr::new(addr, port + i); 
+        let sa = SocketAddr::new(addr, port + i);
         match TcpStream::connect_timeout(&sa, std::time::Duration::new(0, timeout)) {
             Ok(_) => {
                 print!("{}", Color::Green.paint("."));
                 io::stdout().flush().unwrap();
                 tx.send(std::format!("{} is open.", port + i)).unwrap();
-            },
-            Err(e) => {
-                match e.kind() {
-                    io::ErrorKind::TimedOut =>  {
-                        if verbosity >= 1 {
-                            print!("{}", Color::Red.paint("."));
-                            io::stdout().flush().unwrap();
-                        }
-                        tx.send(std::format!("port {} timed out.", port + i)).unwrap();
-                    },
-                    _ => {
-                        if verbosity >= 1 {
-                            print!("{}", Color::Red.paint("."));
-                            io::stdout().flush().unwrap();
-                        }
+            }
+            Err(e) => match e.kind() {
+                io::ErrorKind::TimedOut => {
+                    if verbosity >= 1 {
+                        print!("{}", Color::Red.paint("."));
+                        io::stdout().flush().unwrap();
+                    }
+                    tx.send(std::format!("port {} timed out.", port + i))
+                        .unwrap();
+                }
+                _ => {
+                    if verbosity >= 1 {
+                        print!("{}", Color::Red.paint("."));
+                        io::stdout().flush().unwrap();
                     }
                 }
-            }     
+            },
         }
-
     }
 }
 
 pub fn run(threads: u16, address: IpAddr, timeout: u32, verbose: u8, famous: bool) {
     let (tx, rx) = channel();
-    let number_of_slots = if famous {((FAMOUS / threads) as f64).ceil() as u16} else {((MAX / threads) as f64).ceil() as u16};
-    for i in 0..threads{
+    let number_of_slots = if famous {
+        ((FAMOUS / threads) as f64).ceil() as u16
+    } else {
+        ((MAX / threads) as f64).ceil() as u16
+    };
+    for i in 0..threads {
         let tx = tx.clone();
         thread::spawn(move || {
-            scan(tx, i , number_of_slots, address, timeout, verbose, famous);
+            scan(tx, i, number_of_slots, address, timeout, verbose, famous);
         });
     }
     let mut out = vec![];
     drop(tx);
     for p in rx {
         out.push(p);
-    }    
+    }
     out.sort();
     println!("\n{}", Color::Cyan.paint("RESULTS: "));
-    let mut count : u16 = 0;
+    let mut count: u16 = 0;
     for v in out {
         if v.contains("timed") {
             count += 1;
@@ -69,7 +79,11 @@ pub fn run(threads: u16, address: IpAddr, timeout: u32, verbose: u8, famous: boo
         println!("{}", Color::Green.paint(format!("{}", v).as_str()));
     }
     if verbose >= 1 {
-        println!("{}", Color::Red.bold().paint(format!("{} ports timed out.", count).as_str()));
+        println!(
+            "{}",
+            Color::Red
+                .bold()
+                .paint(format!("{} ports timed out.", count).as_str())
+        );
     }
 }
-
